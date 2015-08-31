21e33ff8fd827c1fd214a5f576689765637bcdd4
Neil <nsupnekar@gmail.com>
2015-08-31 14:20:21 -0700

    8===D

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,17 @@ class User < ActiveRecord::Base
   def full_name
   	first_name + " " + last_name
   end
+
+  validates :first_name, presence: true
+
+  validates :last_name, presence: true
+
+  validates :profile_name, presence: true, uniqueness: true,
+  							format:  {
+  								with: /a-zA-Z0-9_-/,
+  								message: 'Must be formatted correctly.'
+  							}
+
+  
+  has_many :statuses
 end

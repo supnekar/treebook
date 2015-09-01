6e2ef9fca4062297edc4040b02bb18757ed74f19
Neil <nsupnekar@gmail.com>
2015-09-01 15:32:44 -0700

    done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+
   devise_for :users
 
   devise_scope :user do
@@ -12,7 +14,7 @@ Rails.application.routes.draw do
   get 'feed', to: 'statuses#index', as: :feed
   root to: 'statuses#index'
 
-  
+  get '/:id', to: 'profiles#show'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

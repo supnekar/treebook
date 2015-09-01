c68496a948ce130fea776132ec9f7c039ef3293b
Neil <nsupnekar@gmail.com>
2015-09-01 10:17:45 -0700

    ewdfsdfds

--- test/controllers/statuses_controller_test.rb
+++ test/controllers/statuses_controller_test.rb
@@ -17,6 +17,21 @@ class StatusesControllerTest < ActionController::TestCase
     assert_redirected_to new_user_session_path
   end
 
+  test "should create status when logged in" do
+    sign_in users(:jason)
+    
+    assert_difference('Status.count') do
+        post :create, status: { content: @status.content }
+      end
+  
+
+
+  test "should be logged in to post a status" do
+  post :create, status: {content: "hello"}
+  assert_response :redirect
+  assert_redirected_to new_user_session_path
+end
+
   test "should render the new page when logged in" do
     sign_in users(:jason)
     get :new
--- test/models/status_test.rb
+++ test/models/status_test.rb
@@ -4,4 +4,23 @@ class StatusTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "that a status requires content" do
+  	status = Status.new
+  	assert !status.save
+  	assert !status.errors[:content].empty?
+  end
+
+  test "that a status is at least 2 letters long" do
+  	status = Status.new
+  	status.content = "H"
+  	assert !status.save
+  	assert !status.errors[:content].empty?
+  end
+
+  test "That a status has a user ID" do
+  	status = Status.new
+  	status.content = "Hello"
+  	assert !status.save
+  	assert !status.errors[:user_id].empty?
+  end
 end

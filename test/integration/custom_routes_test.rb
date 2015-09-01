f3d7221f1bde65b89ec048eae9efd813df38b6d8
Neil <nsupnekar@gmail.com>
2015-09-01 09:52:51 -0700

    ok ew file:   test/integration/custom_routes_test.rb

---
+++ test/integration/custom_routes_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+
+class CustomRoutesTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  test "that /login route opens login page" do
+  	get '/login'
+  	assert_response :success
+  end
+
+    test "that /logout route opens logout page" do
+  	get '/logout'
+  	assert_response :redirect
+  	assert_redirected_to '/'
+  end
+
+    test "that /register route opens register page" do
+  	get '/register'
+  	assert_response :success
+  end
+end

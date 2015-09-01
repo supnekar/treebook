ed0b2e5578dc7db9eee85ee9da5754eaa5944171
Neil <nsupnekar@gmail.com>
2015-09-01 13:05:12 -0700

    jweijfsdijfijfds

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -31,10 +31,18 @@ end
 end
 
 test "a user should have a profile name without spaces" do
-	user = User.new
+	user = User.new(first_name: 'Jason', last_name: 'Seifer', email: 'jason2@teamtreehouse.com')
+	user.password = user.password_confirmation = 'asdf1sdf'
 	user.profile_name = "My profile name with spaces"
 	assert !user.save
 	assert !user.errors[:profile_name].empty?
 	assert user.errors[:profile_name].include?("Must be formatted correctly.")
+end
+
+test "a user can have a correctly formatted profile name" do
+	user = User.new(first_name: 'Jason', last_name: 'Seifer', email: 'jason2@teamtreehouse.com')
+	user.password = user.password_confirmation = 'asdf1sdf'
+	user.profile_name = 'ja1son'
+	assert user.valid?
 end
 end
